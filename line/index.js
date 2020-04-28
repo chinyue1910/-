7f254818a2c3fcf434d6bf36419fd795de959457
chinyue1910 <asd874631@gmail.com>
2020-04-28 11:48:51 +0800

    註解更動完成

--- line/index.js
+++ line/index.js
@@ -3,6 +3,6 @@
 // npm install linebot LINE 機器人
 // package-lock.json 更詳細的套件內容，不要去動他
 // .gitignore 忽略要上傳到 git hub 的內容
-// "dev": "nodemon index.js" 讓網頁會自動重新整理
+// 在 scripts 裡新增 "dev": "nodemon index.js" 讓網頁會自動重新整理
 // npm install -g eslint 安裝 node.js eslint 讓程式碼格式化
 // npm install 安裝所缺的套件
