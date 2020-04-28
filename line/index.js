d720f89dd51b085afa1de1b01bfbb7b844d6d2dc
chinyue1910 <asd874631@gmail.com>
2020-04-28 13:17:54 +0800

    設定 格式

--- line/index.js
+++ line/index.js
@@ -6,3 +6,11 @@
 // 在 scripts 裡新增 "dev": "nodemon index.js" 讓網頁會自動重新整理
 // npm install -g eslint 安裝 node.js eslint 讓程式碼格式化
 // npm install 安裝所缺的套件
+
+// 引用 linebot 套件
+import linebot from 'linebot'
+// 引用 dotenv 套件
+import dotenv from 'dotenv'
+
+// 讀取 .env 檔
+dotenv.config()
