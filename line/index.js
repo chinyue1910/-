902af08fddb0d632b55af6c1a9dd847066a2cdee
chinyue1910 <asd874631@gmail.com>
2020-05-07 00:17:14 +0800

    初步設置完成

--- line/index.js
+++ line/index.js
@@ -1,7 +1,6 @@
 import linebot from 'linebot'
 import dotenv from 'dotenv'
-import rp from 'request-promise'
-import search from 'youtube-search'
+import YouTube from 'youtube-node'
 
 dotenv.config()
 
@@ -11,41 +10,23 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
-const opts = {
-  maxResults: 1,
-  key: process.env.API_KEY
+const youTube = new YouTube()
+youTube.setKey(process.env.API_KEY)
+
+const test = async () => {
+  await youTube.search('蔡阿嘎', 2, function (error, result) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log(JSON.stringify(result, null, 2))
+    }
+  })
 }
 
-// const test = async () => {
-//   let msg = ''
-//   try {
-//     await search('蔡阿嘎', opts, function (err, results) {
-//       if (err) return console.log(err)
-//       for (let i of results) {
-//         console.dir(i.thumbnails.high.url)
-//       }
-//     })
-//   } catch (error) {
-//     msg = '錯誤'
-//   }
-//   console.log(msg)
-// }
-
-// test()
-
-bot.on('message', async (event) => {
-  let msg = '123'
-  await search('蔡阿嘎', opts, function (err, results) {
-    if (err) { return console.log(err) }
-    // for (const i of results) {
-    //   console.log(i.title)
-    // }
-    msg = results[0].title
-    console.log(msg)
-    return event.reply(msg)
-  })
-})
+test()
 
 bot.listen('/', process.env.PORT, () => {
   console.log('已啟動')
 })
+
+// https://www.npmjs.com/package/youtube-node 套件用法
